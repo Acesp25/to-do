d1d2452d82e8da57e40f141249be05e2f962fe7a
Acespinoza <rii11@txstate.edu>
2025-03-13 12:59:22 -0500

    Move file operations to planner.rs might undo this

--- src/backend/mod.rs
+++
@@ -1,3 +0,0 @@
-pub mod classes;
-pub mod enums;
-pub mod file_read_write;
--- src/ui/cli.rs
+++ src/ui/cli.rs
@@ -1,10 +1,8 @@
 use crate::ui::ui_helpers::{display_menu, get_choice, event_creater, adjust_event};
-use crate::backend::file_read_write::load_events;
 use crate::backend::classes::planner::{self, Planner};
 use chrono::Local;
 
 pub fn start_ui() {
-    load_events();
 
     let mut planner = Planner::new("Test Planner".to_string());
 
