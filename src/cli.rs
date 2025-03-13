cde695b3cd27512a6607838b331f46ac2b65f208
Acespinoza <rii11@txstate.edu>
2025-03-12 23:49:35 -0500

    Created display_menu function

---
+++ src/cli.rs
@@ -0,0 +1,9 @@
+pub fn display_menu() {
+    println!("  Rust To-Do Planner!");
+    println!("      1. Display today's plans");
+    println!("      2. Create an event");
+    println!("      3. Adjust an event");
+    println!("      4. Display ALL events");
+    println!("      5. Exit");
+}
+
