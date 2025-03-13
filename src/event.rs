bc8f9830ab1461077fd368fd7d6f23b5ea60ab72
Acespinoza <rii11@txstate.edu>
2025-03-12 23:29:37 -0500

    Inital Commit

---
+++ src/event.rs
@@ -0,0 +1,105 @@
+use chrono::NaiveDateTime;
+
+#[derive(Debug, Clone)]
+pub enum Priority {
+    High,
+    Medium,
+    Low,
+}
+
+#[derive(Debug, Clone)]
+pub enum Reoccurance {
+    Yearly,
+    Monthly,
+    Fornite,
+    Weekly,
+    Daily,
+    None,
+}
+
+#[derive(Debug, Clone)]
+pub struct Event {
+    name: String,
+    start_time: NaiveDateTime,
+    end_time: NaiveDateTime,
+    priority: Priority,
+    reoccurance: Reoccurance,
+    note: String,
+    completed: bool,
+}
+
+impl Event {
+    // Constructor
+    pub fn new(
+        name: String,
+        start_time: NaiveDateTime,
+        end_time: NaiveDateTime,
+        priority: Priority,
+        reoccurance: Reoccurance,
+        note: String,
+        completed: bool,
+    ) -> Self {
+        Self {
+            name,
+            start_time,
+            end_time,
+            priority,
+            reoccurance,
+            note,
+            completed,
+        }
+    }
+    
+    // Helpers
+    pub fn display(&self) {
+        println!(" __________________________________________");
+        println!("| Event: {:<30} |", self.name);
+        println!("| Start Time: {:<24} |", self.start_time);
+        println!("| End Time: {:<24} |", self.end_time);
+        println!("| Priority: {:<30} |", format!("{:?}", self.priority));
+        println!("| Reoccurance: {:<26} |", format!("{:?}", self.reoccurance));
+        println!("| Note: {:<30} |", self.note);
+        println!("| Completed: {:<27} |", self.completed);
+        println!(" __________________________________________");
+    }
+
+    // Getters
+    pub fn get_name(&self) -> &String {
+        &self.name
+    }
+    pub fn get_start_time(&self) -> &NaiveDateTime {
+        &self.start_time
+    }
+    pub fn get_end_time(&self) -> &NaiveDateTime {
+        &self.end_time
+    }
+    pub fn get_reoccurance(&self) -> &Reoccurance {
+        &self.reoccurance
+    }
+    pub fn get_note(&self) -> &String {
+        &self.note
+    }
+    pub fn get_completed(&self) -> bool {
+        self.completed
+    }
+
+    // Setters
+    pub fn set_name(&mut self, new_name: String) {
+        self.name = new_name;
+    }
+    pub fn set_start_time(&mut self, new_start_time: NaiveDateTime) {
+        self.start_time = new_start_time;
+    }
+    pub fn set_end_time(&mut self, new_end_time: NaiveDateTime) {
+        self.end_time = new_end_time;
+    }
+    pub fn set_reoccurance(&mut self, new_reoccurance: Reoccurance) {
+        self.reoccurance = new_reoccurance;
+    }
+    pub fn set_note(&mut self, new_note: String) {
+        self.note = new_note;
+    }
+    pub fn set_completed(&mut self, new_completed: bool) {
+        self.completed = new_completed;
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,25 @@
+use chrono::NaiveDateTime;
+use crate::event::{Event, Priority, Reoccurance};
+
+mod event;
+fn main() {
+    let dummy_time = NaiveDateTime::from_timestamp(1_600_000_000, 0);
+
+    let mut e1 = Event::new(
+        "Test Event".to_string(),
+        dummy_time,
+        dummy_time,
+        Priority::Medium,
+        Reoccurance::None,
+        "This is a test note.".to_string(),
+        false,
+    );
+
+    e1.display();
+
+    println!("Original name: {}", e1.get_name());
+    e1.set_name("Updated Event Name".to_string());
+    println!("Updated name: {}", e1.get_name());
+
+    e1.display();
+}
