40273f207cec5b99183605fef0b03e02ce8f9cce
Acespinoza <rii11@txstate.edu>
2025-03-13 11:49:40 -0500

    Main now calls the ui

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,4 @@
-use chrono::NaiveDateTime;
-use crate::backend::classes::planner::Planner;
-use crate::backend::classes::event::Event;
-use crate::backend::enums::reoccurance::Reoccurance;
-use crate::backend::enums::priority::Priority;
+use crate::ui::cli::start_ui;
 
 mod backend {
     pub mod classes;
@@ -11,24 +7,5 @@ mod backend {
 mod ui;
 
 fn main() {
-    let dummy_time = NaiveDateTime::from_timestamp(1_600_000_000, 0);
-
-    let mut e1 = Event::new(
-        0,
-        "Test Event".to_string(),
-        dummy_time,
-        dummy_time,
-        Priority::Medium,
-        Reoccurance::None,
-        "This is a test note.".to_string(),
-        false,
-    );
-
-    e1.display();
-
-    println!("Original name: {}", e1.get_name());
-    e1.set_name("Updated Event Name".to_string());
-    println!("Updated name: {}", e1.get_name());
-
-    e1.display();
+    start_ui();
 }
