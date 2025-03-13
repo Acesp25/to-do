05640e0d38f5a3889057dffdefb263dac0ac8013
Acespinoza <rii11@txstate.edu>
2025-03-13 10:02:09 -0500

    Added id_input function

--- src/ui/ui_helpers.rs
+++ src/ui/ui_helpers.rs
@@ -1,3 +1,6 @@
+use std::io::{self, Write};
+use crate::planner::Planner;
+
 pub fn display_menu() {
     println!("  Rust To-Do Planner!");
     println!("      1. Display today's plans");
@@ -13,7 +16,29 @@ pub fn create_event() {
 
 pub fn adjust_menu(planner: Planner) {
     println!("  Select an event to edit! (enter eventID)");
+    planner.list_events(planner);
 
-    
 
+    if let Some(event) = planner.find_event_mut(event_id) {
+
+    }
+}
+
+pub fn id_input() -> Option<usize>{
+    print!("> ");
+    io::stdout().flush().expect("Failed to flush stdout");
+
+    let mut input = String::new();
+    if let Err(e) = io::stdin().read_line(&mut input) {
+        println!("Error reading input: {}", e);
+        return;
+    }
+
+    match input.trim().parse::<usize>() {
+        Ok(id) => Some(id),
+        Err(_) => {
+            println!("Invalid event ID entered.");
+            None
+        }
+    }
 }
